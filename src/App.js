6bf40d7e138720a5765fc6844e1013b5a59d283b
Truong Thai <52000714@student.tdtu.edu.vn>
2023-04-01 22:57:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Photos from "./Components/Photo/Photos";
-
+// import React from "react";
 // import "./App.css";
 import "./index.scss";
 
 function App() {
   return (
     <div>
+    
     <Photos></Photos>
    
     </div>
--- src/Components/Photo/Photos.js
+++ src/Components/Photo/Photos.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const getPhoto = () => {
   return axios
-    .get("https://picsum.photos/v2/list")
+    .get("https://picsum.photos/v2/list?page=2&limit=8")
     .then(function (response) {
       //   console.log(response.data);
       return response.data;
@@ -19,14 +19,15 @@ const Photos = () => {
   useEffect(() => {
     getPhoto().then((response) => {
       console.log(response);
-      setPhoto(response);
+      setPhoto(response);// sau doan nay sex rerender lai code nen se console.log 2 lan 
+      // va useState su dung la useState([]) ne khong rerender lai code
     });
   }, []);
 
   return (
     <div className="grid grid-cols-4 gap-5 p-5">
       {photo.length > 0 && photo.map((item, index) => (
-        <div key={item.id} className="p-3 bg-white shadow-md rounded-lg">
+        <div key={item.id} className="p-3 bg-white shadow-md rounded-lg h-[300px]">
           <img src={item.download_url} alt={item.author} className="w-full h-full object-cover rounded-lg"/>
         </div>
       ))}
